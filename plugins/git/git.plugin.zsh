a8f84b990b79baae34a314b588caee4b20687a7b
ftorto <dev.torto@gmail.com>
2019-04-03 18:06:27 +0200

    deactivate alias gr

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -190,7 +190,7 @@ alias gpoat='git push origin --all && git push origin --tags'
 alias gpu='git push upstream'
 alias gpv='git push -v'
 
-alias gr='git remote'
+#alias gr='git remote'
 alias gra='git remote add'
 alias grb='git rebase'
 alias grba='git rebase --abort'
